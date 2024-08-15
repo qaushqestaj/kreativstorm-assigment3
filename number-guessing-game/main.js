7498326d36e32654a4c62d90acb409ab50c2c1c9
qaushqestaj <qaushqestaj@hotmail.com>
2024-08-15 18:22:06 +0200

    updated scoring system

--- number-guessing-game/main.js
+++ number-guessing-game/main.js
@@ -1,4 +1,4 @@
-const generateRandomNumber = () => Math.floor(Math.random() * 100) + 1; // Generate a random number between 1 and 10
+const generateRandomNumber = () => Math.floor(Math.random() * 100) + 1; // Generate a random number between 1 and 100
 
 // Get the number that player enters
 function getPlayerGuess() {
@@ -52,51 +52,64 @@ function game() {
   const randomNumber = generateRandomNumber(); // Store the generated random number in a variable
   let playerGuess;
   let attempts = 0;
-  let score = 0;
   const maxAttempts = 10;
 
   while (attempts < maxAttempts) {
     let playerGuess = getPlayerGuess();
-    attempts++;
 
     // Only check if the player didn't cancel the prompt
     if (playerGuess == undefined) {
       let score = 0;
-      console.log(`You canceled the game. Your bonus score is ${score}`);
+      console.log(`You canceled the game. Your bonus score is ${score}.`);
       break;
     }
 
+    attempts++;
     const result = checkGuess(playerGuess, randomNumber); // Store the result of checkGuess function
     console.log(result);
+
+    const scoringSystem = {
+      attempts:
+        attempts == 1
+          ? (score = 100)
+          : attempts == 2
+          ? (score = 90)
+          : attempts == 3
+          ? (score = 80)
+          : attempts == 4
+          ? (score = 70)
+          : attempts == 5
+          ? (score = 60)
+          : attempts == 6
+          ? (score = 50)
+          : attempts == 7
+          ? (score = 40)
+          : attempts == 8
+          ? (score = 30)
+          : attempts == 9
+          ? (score = 20)
+          : attempts == 10
+          ? (score = 10)
+          : (score = 0),
+
+      grade:
+        attempts === 100
+          ? 'Excellent 👏'
+          : attempts <= 50
+          ? 'Nice, at least you found it 😒'
+          : attempts > 50
+          ? 'Very nice 👍'
+          : 'Really? You could not guess that at all 🤦‍♂️?',
+    };
 
     // If the user win
     if (result === 'The number you entered is correct!') {
       alert(
-        `Congratulations! You guessed the correct number in ${attempts} attempts. Your bonus score is ${
-          attempts == 1
-            ? (score = 100)
-            : attempts == 2
-            ? (score = 90)
-            : attempts == 3
-            ? (score = 80)
-            : attempts == 4
-            ? (score = 70)
-            : attempts == 5
-            ? (score = 60)
-            : attempts == 6
-            ? (score = 50)
-            : attempts == 7
-            ? (score = 40)
-            : attempts == 8
-            ? (score = 30)
-            : attempts == 9
-            ? (score = 20)
-            : attempts == 10
-            ? (score = 10)
-            : (score = 0)
-        }.`
+        `Congratulations! You guessed the correct number in ${attempts} attempts. Your bonus score is ${scoringSystem.attempts} and your grade is ${scoringSystem.grade}`
       );
-      console.log(`Wohoo you have won! 🎉🎉. Your bonus score is ${score}.`);
+      console.log(
+        `Wohoo you have won! 🎉🎉. Your bonus score is ${scoringSystem.attempts}. Your grade is ${scoringSystem.grade}`
+      );
       break;
     } else {
       alert(result + ' Try again');
